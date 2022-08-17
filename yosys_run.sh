e587030dd04847a2eb4c32006e07aefe11e1f0e8
YASHWANT98 <60091654+YASHWANT98@users.noreply.github.com>
2022-08-18 00:39:57 +0530

    Update yosys_run.sh

--- yosys_run.sh
+++ yosys_run.sh
@@ -1,17 +1,16 @@
-git clone https://github.com/YASHWANT98/iiitb_r2_4bit_bm
-
-cd iiitb_r2_4bit_bm
-
-yosys
-
+# Read liberty file
 read_liberty -lib /home/yashm98/iiitb_r2_4bit_bm/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
 
+#Read verilog file
 read_verilog iiitb_r2_4bit_bm.v
 
+# Synthesis
 synth -top iiitb_r2_4bit_bm
 
+# Physical Cell Mapping
 abc -liberty /home/yashm98/iiitb_r2_4bit_bm/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
 
+# Write Post Synthesis verilog file
 write_verilog iiitb_r2_4bit_bm_postsyn.v
 
 show -stretch -width iiitb_r2_4bit_bm
